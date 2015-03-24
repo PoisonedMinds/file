31e5d04ad810ce7b78639e157ffbae44200468ce
PoisonedMinds <steven_biro@hotmail.com>
2015-03-24 12:33:43 -0400

    header

--- file/src/file/fileRead.java
+++ file/src/file/fileRead.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package file;
 
 import java.io.BufferedReader;
@@ -12,8 +8,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 /**
- *
  * @author Steven Biro
+ * @teacher Mr. J. Carron
+ * @date 11-Mar-2015 10:51:46 AM
  */
 public class fileRead {
 
