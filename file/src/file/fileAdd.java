c885444bef7fc0acc3ef4ac6a47ee115fb8b290f
PoisonedMinds <steven_biro@hotmail.com>
2015-03-24 12:33:38 -0400

    header

--- file/src/file/fileAdd.java
+++ file/src/file/fileAdd.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package file;
 
 import java.io.FileWriter;
@@ -13,8 +9,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
  * @author Steven Biro
+ * @teacher Mr. J. Carron
+ * @date 11-Mar-2015 10:51:46 AM
  */
 public class fileAdd {
 
